d5adecf6b2e27651cd6ae093fc7e1c77ecdb4059
Jixian Li <ljx.sci@gmail.com>
2026-05-28 22:37:55 -0500

    Sync with /scratch/projects/tacc/sci-training/cosmicai_2026/install.sh; added cuda 131 to modules; changed ucc/2.7 to ucc/1.7 (on Vista:/scratch, not here)

--- cosmicai_26/install.sh
+++ cosmicai_26/install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
 module purge
 
 # Load specific modules to match the exact target environment
-module load ucc/1.7.0 ucx/1.20.0 cmake/4.1.1 TACC gcc/15.1.0 nvpl/26.1 openmpi/5.0.5 python3/3.11.8 sqlite/3.46.1
+module load ucc/1.7.0 ucx/1.20.0 cmake/4.1.1 TACC gcc/15.1.0 openmpi/5.0.5 python3/3.11.8 sqlite/3.46.1 cuda/13.1
 
 # ---- Installing Jupyter Kernel ----
 
@@ -27,3 +26,33 @@ if [ -e $NEW_DIR ]
    # copy materials to local tutorial directory
    cp -r $SRC_DIR $DST_DIR
 fi
+
+# ---- Copying Notebook and Materials to $HOME ----
+
+# NOTE: to be updated once we have a
+
+#path to folder that holds the notebook and other materials
+SRC_DIR=/scratch/projects/tacc/sci-training/sci_tacc_education_materials/cosmicai_26
+
+# path where tutorial content saved to
+DST_DIR=$HOME
+
+# newly create directory if copy is successful
+NEW_DIR=${DST_DIR}/tutorial_spring_2026
+
+
+if [ -e $NEW_DIR ]
+   then
+    echo "course material already copied"
+   else
+    # make tutorial directory
+    mkdir -p $DST_DIR
+    # copy materials to local tutorial directory
+    cp -r $SRC_DIR $DST_DIR
+fi
+
+# reset users default environment
+#module reset
+#module load gcc python3
+#module save
+#echo "default environment changed to include gcc and python3"
